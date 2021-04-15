9a5ae71b551d46ad912a08efd7c1a9c7213671ad
Jelailah <anofiujelailah@gmail.com>
2021-04-15 21:00:30 +0100

    Update filterer.py

--- filterer.py
+++ filterer.py
@@ -21,7 +21,7 @@ def extract_trends(BEARER_TOKEN):
     return all_trends
 
 def collect_trend(tweet, all_trends):
-    tweet = tweet.replace("@filtertrends ", "")
+    tweet = tweet.replace("@filtertrend ", "")
     if tweet in all_trends:
         all_trends.remove(tweet)
     return [all_trends, tweet]
